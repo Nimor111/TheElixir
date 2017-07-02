41baca8f5b864342c84ca4f6884a7f1a0a3e36f9
Nimor111 <georgi.bojinov@hotmail.com>
2017-07-03 00:45:53 +0300

    Fix path in tests

--- test/inventory_test.exs
+++ test/inventory_test.exs
@@ -1,36 +1,38 @@
 defmodule TheElixirInventoryTest do
   use ExUnit.Case, async: true
 
+  alias TheElixir.Components.Inventory
+
   setup context do
-    {:ok, _} = TheElixir.Inventory.start_link(context.test)
+    {:ok, _} = Inventory.start_link(context.test)
     {:ok, inventory: context.test}
   end
 
   test "adds item", %{inventory: inventory} do
-    assert TheElixir.Inventory.lookup(inventory, "head") == :error
+    assert Inventory.lookup(inventory, "head") == :error
 
-    TheElixir.Inventory.add(inventory, "head", "helm of might")
-    assert {:ok, _} = TheElixir.Inventory.lookup(inventory, "head")
+    Inventory.add(inventory, "head", "helm of might")
+    assert {:ok, _} = Inventory.lookup(inventory, "head")
   end
 
   test "deletes item", %{inventory: inventory}  do
-    TheElixir.Inventory.add(inventory, "head", "helm of might")
-    assert {:ok, _} = TheElixir.Inventory.lookup(inventory, "head")
+    Inventory.add(inventory, "head", "helm of might")
+    assert {:ok, _} = Inventory.lookup(inventory, "head")
 
-    TheElixir.Inventory.delete(inventory, "head")
+    Inventory.delete(inventory, "head")
     # bogus call so DOWN is processed
-    TheElixir.Inventory.add(inventory, "legs", "pants of might")
-    assert TheElixir.Inventory.lookup(inventory, "head") == :error
+    Inventory.add(inventory, "legs", "pants of might")
+    assert Inventory.lookup(inventory, "head") == :error
   end
 
   test "process is restarted correctly", %{inventory: inventory} do
-    TheElixir.Inventory.add(inventory, "head", "helm of might")
+    Inventory.add(inventory, "head", "helm of might")
 
-    assert {:ok, _} = TheElixir.Inventory.lookup(inventory, "head")
+    assert {:ok, _} = Inventory.lookup(inventory, "head")
 
-    TheElixir.Inventory.stop(inventory)
+    Inventory.stop(inventory)
     :timer.sleep(2)
     # this is retarded
-    assert TheElixir.Inventory.lookup(:inventory, "head") == :error
+    assert Inventory.lookup(:inventory, "head") == :error
   end
 end
