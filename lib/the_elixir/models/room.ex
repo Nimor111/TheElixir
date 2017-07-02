5ff591df0f39d8c642f0789c7b68d2183f1b3da0
Nimor111 <georgi.bojinov@hotmail.com>
2017-07-02 16:07:12 +0300

    Add some docs and methods to Room

--- lib/the_elixir/models/room.ex
+++ lib/the_elixir/models/room.ex
@@ -5,11 +5,18 @@ defmodule TheElixir.Models.Room do
 
   alias TheElixir.Models.Room, as: Room
   
-  defstruct [:name, :max_questions]
-
-  def new(name, max_questions \\ 20) do
-    %Room{name: name, max_questions: max_questions}
+  defstruct [:name, :quests]
+  
+  @doc """
+  Create a new room.
+  `name` : name of the room
+  `quests` : the quests to be completed in the room to exit
+  """
+  def new(name, quests \\ []) do
+    %Room{name: name, quests: quests}
   end
 
-  def add_question(question) do; end
+  def add_quest(room, quest) do
+    [quest | room.quests]
+  end
 end
