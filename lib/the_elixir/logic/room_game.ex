b4e1a8459e6181557809e4c2f725406ada1f8bda
Nimor111 <georgi.bojinov@hotmail.com>
2017-07-07 11:10:55 +0300

    Add more options to room menu

--- lib/the_elixir/logic/room_game.ex
+++ lib/the_elixir/logic/room_game.ex
@@ -6,7 +6,9 @@ defmodule TheElixir.Logic.RoomGame do
   alias TheElixir.Logic.RoomGame 
   alias TheElixir.Components.World
   alias TheElixir.Components.Journal
+  alias TheElixir.Components.Inventory
   alias TheElixir.Logic.Game
+  alias TheElixir.Lobby
 
   def pick_room(player, room_name) do
     # TODO add progress logic
@@ -26,9 +28,49 @@ defmodule TheElixir.Logic.RoomGame do
     player |> RoomGame.get_input(room)
   end
 
+  def command_help(player, room) do
+    IO.puts([
+      "m -> move forward\n",
+      "i -> inspect surroundings\n",
+      "e -> exit / quit\n",
+      "inv -> view inventory\n",
+      "world -> view all rooms in the world\n",
+      "q -> view quests in the room\n",
+      "add -> add a quest to your journal\n",
+      "j -> view journal\n",
+      "c -> clear screen\n"
+    ])
+    player |> RoomGame.get_input(room)
+  end
+
+  def get_journal(player, room) do
+    quests = Journal.get(:journal)
+    case quests do
+      [] -> IO.puts("No entries yet.")
+      _  -> IO.puts(quests)
+    end
+    player |> RoomGame.get_input(room)
+  end
+
   def get_input(player, room) do
     input = IO.gets("(press h for help) >> ") |> String.strip
     player |> RoomGame.match_input(input, room)
+  end
+
+  def clear_screen(player, room) do
+    IO.puts("Clearing screen...")
+    :timer.sleep(1000)
+    System.cmd "clear", [], into: IO.stream(:stdio, :line)
+    player |> RoomGame.get_input(room)
+  end
+  
+  def get_inventory(player, room) do
+    items = Inventory.get(:inventory)
+    case items do
+      [] -> IO.puts("No items yet.")
+      _  -> IO.puts(items)
+    end
+    player |> RoomGame.get_input(room)
   end
 
   def match_input(player, input, room) do
@@ -38,7 +80,15 @@ defmodule TheElixir.Logic.RoomGame do
       "add" ->
         player |> RoomGame.add_quest(room)
       "j" ->
-        Game.get_journal(player)
+        player |> RoomGame.get_journal(room)
+      "h" ->
+        player |> RoomGame.command_help(room)
+      "e" ->
+        Lobby.exit
+      "c" ->
+        player |> RoomGame.clear_screen(room) 
+      "inv" ->
+        player |> RoomGame.get_inventory(room)
       _ ->
         IO.puts "We don't know this command ( yet ). Read the prompt!"
         player |> RoomGame.get_input(room)
