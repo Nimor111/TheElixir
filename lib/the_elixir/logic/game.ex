dc1304eb92514351c9169c326ecc393a79675d2b
Nimor111 <georgi.bojinov@hotmail.com>
2017-07-07 11:28:13 +0300

    Add documentation to game

--- lib/the_elixir/logic/game.ex
+++ lib/the_elixir/logic/game.ex
@@ -9,7 +9,11 @@ defmodule TheElixir.Logic.Game do
   alias TheElixir.Components.Inventory
   alias TheElixir.Components.Journal
   alias TheElixir.Logic.RoomGame
-
+  
+  @doc """
+  On key press `h`
+  List of commands that `player` can execute
+  """
   def command_help(player) do
     IO.puts([
       "m -> move forward\n",
@@ -24,11 +28,18 @@ defmodule TheElixir.Logic.Game do
     Game.get_input(player)
   end
 
+  @doc """
+  Function that takes the `player`'s next input and sends it
+  to be interpreted
+  """
   def get_input(player) do
     input = IO.gets("(press h for help) >> ") |> String.strip
     player |> Game.match_input(input)
   end
 
+  @doc """
+  Function that interprets `player` `input`
+  """
   def match_input(player, input) do
     rooms = World.get(:world)
     case input do
@@ -46,6 +57,10 @@ defmodule TheElixir.Logic.Game do
     end 
   end
 
+  @doc """
+  Clears the screen for better vision after a second delay
+  On key press `c`
+  """
   def clear_screen(player) do
     IO.puts("Clearing screen...")
     :timer.sleep(1000)
@@ -53,6 +68,10 @@ defmodule TheElixir.Logic.Game do
     Game.get_input(player)
   end
 
+  @doc """
+  Gets the current quests in the `player` journal
+  On key press `j`
+  """
   def get_journal(player) do
     quests = Journal.get(:journal)
     case quests do
@@ -62,6 +81,10 @@ defmodule TheElixir.Logic.Game do
     Game.get_input(player)
   end
 
+  @doc """
+  Gets the current items in the `player` inventory
+  On key press `inv`
+  """
   def get_inventory(player) do
     items = Inventory.get(:inventory)
     case items do
@@ -71,6 +94,10 @@ defmodule TheElixir.Logic.Game do
     Game.get_input(player)
   end
 
+  @doc """
+  Gets the current rooms in the world
+  On key press `world`
+  """
   def get_rooms(player) do
     rooms = World.get(:world)
     case rooms do
@@ -81,6 +108,9 @@ defmodule TheElixir.Logic.Game do
     Game.get_input(player)
   end
  
+  @doc """
+  The text that pops the first time you enter the game
+  """
   def game_introduction(player) do
     IO.puts([
       "|>|>|>|>|>|>|>|>|>|>|>|>\n",
@@ -93,6 +123,9 @@ defmodule TheElixir.Logic.Game do
     Game.hallway_introduction(player)
   end
 
+  @doc """
+  The hallway introduction text, notice the |>
+  """
   def hallway_introduction(player) do
     IO.puts([ 
       "You find yourself in a hallway, the walls are lined with symbols.\n",
@@ -105,16 +138,28 @@ defmodule TheElixir.Logic.Game do
     Game.get_input(player)
   end
 
+  @doc """
+  Move the player forward. Currently doesn't do much.
+  On key press `m`
+  """
   def move(player) do
     IO.puts("You moved forward.")
     Game.get_input(player)
   end
 
+  @doc """
+  Inspect your surroundings. Notices doors, mostly.
+  On key press `i`
+  """
   def inspect(player) do
     IO.puts("There is a door next to you. Maybe it leads somewhere?")
     Game.get_input(player)
   end
 
+  @doc """
+  Choose a room, takes first one for now
+  On key press `w`
+  """
   def room(rooms, _) when rooms == [], do: "There is no room nearby!"
   def room(rooms, player) do
     [ room_name | _  ] = rooms
